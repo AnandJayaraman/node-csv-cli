634f41328ba424445c417ea67c0e205ac2839755
AnandJayaraman <anand201301@gmail.com>
2021-08-11 19:47:07 +0530

    Making the syntax better as per JS norms

---
+++ backend/index.js
@@ -0,0 +1,43 @@
+#! /usr/bin/env node
+console.log("Hello World!");
+//cli(process.argv);
+// Argument Validation
+// Record Validation
+// DB Insert
+args=process.argv;
+( function cli(args=process.argv){
+    const Fs = require('fs');
+    const db = require("../src/models");
+    const moment = require('moment');
+
+    const jobList = db.Jobslist;
+    const CsvReadableStream = require('csv-reader');
+    let tempJobList = [];
+    //./data/jobs-list.csv
+    let inputStream = Fs.createReadStream(args[2], 'utf8');
+    
+    inputStream
+        .pipe(new CsvReadableStream({ skipHeader: true, asObject: true, parseNumbers: true, parseBooleans: true, trim: true }))
+        .on('data', function (row) {
+            console.log('A row arrived: ', row);
+            const jobRow = {
+                jobTitle: row["job title"],
+                jobDescription: row["job description"],
+                date: moment(row["date"], 'DD/MM/YYYY'), 
+                applicants: row["applicants"]
+            }
+            tempJobList.push(jobRow);
+        })
+        .on('end', function () {
+            console.log('No more rows!');
+            jobList.bulkCreate(tempJobList)
+            .then(() => {
+                console.log('bulk created');
+              })
+              .catch((error) => {
+                console.log('error',error);
+              });            
+        });
+    // List of Objects to be pushed to DB
+    console.log(args);
+})();
