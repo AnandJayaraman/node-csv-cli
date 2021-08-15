1b7d0927ed0a90011d4dab097a2804b6096d5dfb
AnandJayaraman <anand201301@gmail.com>
2021-08-15 12:50:21 +0530

    Sample Prod code

--- index.js
+++ index.js
@@ -1,57 +1,15 @@
-#! /usr/bin/env node
-console.log("Hello World!");
-//cli(process.argv);
-// Argument Validation
-// Record Validation
-// DB Insert
+var express = require('express');
+var router = express.Router();
+var cors = require('cors');
 
-( function cli(args=process.argv){
-    const Fs = require('fs');
-    const db = require("../src/models");
-    const moment = require('moment');
-    const Validator = require('validatorjs');
+router.use(cors());
 
-    const jobList = db.Jobslist;
-    const CsvReadableStream = require('csv-reader');
-    let tempJobList = [];
-    //./data/jobs-list.csv
+router.get('/', (req, res) => {
+  res.send({ message: "ok" });
+});
 
-    let rules = {
-        jobTitle: 'required|string|between:10,100',
-        jobDescription: 'required|between:10,500',
-        date: 'required|date',
-        applicants:'required|string'
-    };
+router.get('/findAll', (req, res) => {
+  res.send({ message: "executed find all" });
+});
 
-    
-      
-    let inputStream = Fs.createReadStream(args[2], 'utf8');
-    
-    inputStream
-        .pipe(new CsvReadableStream({ skipHeader: true, asObject: true, parseNumbers: true, parseBooleans: true, trim: true }))
-        .on('data', function (row) {
-            console.log('A row arrived: ', row);
-            const jobRow = {
-                jobTitle: row["job title"],
-                jobDescription: row["job description"],
-                date: moment(row["date"], 'DD/MM/YYYY'), 
-                applicants: row["applicants"]
-            }
-            let validation = new Validator(jobRow, rules);  
-            console.log("validation.passes",validation.passes()); // true
-            console.log("validation.fails",validation.fails()); // false                      
-            tempJobList.push(jobRow);
-        })
-        .on('end', function () {
-            console.log('No more rows!');
-            jobList.bulkCreate(tempJobList)
-            .then(() => {
-                console.log('bulk created');
-              })
-              .catch((error) => {
-                console.log('error',error);
-              });            
-        });
-    // List of Objects to be pushed to DB
-    console.log(args);
-})();
+module.exports = router;
